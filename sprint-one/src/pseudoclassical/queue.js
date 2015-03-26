75cb042103630d24d8a6af20557ddb3434e809d4
edoecohen <edoecohen@gmail.com>
2015-03-25 19:23:35 -0700

    Finished sprint #1

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,5 +1,27 @@
 var Queue = function() {
-
+  this.start = 0;
+  this.end = 0;
+  this.storage = {};
 };
+
+
+
+
+
+  Queue.prototype.enqueue = function(item) {
+    this.storage[this.end] = item;
+    this.end++;
+  }
+
+  Queue.prototype.dequeue = function() {
+    if(this.end - this.start > 0){
+      this.start++;
+      return this.storage[this.start-1];
+    }
+  }
+
+  Queue.prototype.size = function() {
+    return this.end - this.start;
+  }
 
 
